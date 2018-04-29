a01efcf01924954daff42d617ce7e0c535dcc193
saya <sa072688@gmail.com>
2018-04-30 06:21:22 +0900

    almost complete ui template

---
+++ Assets/Scripts/UI/ManaBlockMaid.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaBlockMaid : MonoBehaviour
+{
+    public Color NormalColor;
+    public Color CostColor;
+}
